902f7c1222d320ccbbac80d0d8d9dd8c898fbfaa
Arne Vansteenkiste <barnex@gmail.com>
2015-03-31 13:21:44 +0200

    allow w=0

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ var (
 	fl_header    = flag.Bool("h", false, "Output header")
 	fl_npoints   = flag.Int("n", 0, "Number of points")
 	fl_delta     = flag.Float64("d", 1, "Distance between points")
-	fl_clength   = flag.Float64("w", 0, "Correltation length")
+	fl_clength   = flag.Float64("w", 0, "Correlation length")
 	fl_amplitude = flag.Float64("a", 1, "RMS amplitude")
 	fl_kern      = flag.Float64("k", 5, "Kernel cut-off in correlation lengths")
 )
@@ -31,7 +31,7 @@ func main() {
 	width := *fl_clength
 	C := int(*fl_kern * width / delta)
 
-	if N <= 0 || delta <= 0 || width <= 0 {
+	if N <= 0 || delta <= 0 || width < 0 {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -42,29 +42,33 @@ func main() {
 		in[i] = rand.Float64() - 0.5
 	}
 
-	// make kernel
-	klen := 2*C + 1
-	kern := make([]float64, klen)
-	for d := -C; d <= C; d++ {
-		x := (2 * float64(d) * delta / width)
-		kern[C-d] = math.Exp(-x * x)
-	}
-
-	// convolution: out = in * kern
 	out := make([]float64, N)
-	for i := range out {
+	if width != 0 {
+		// make kernel
+		klen := 2*C + 1
+		kern := make([]float64, klen)
 		for d := -C; d <= C; d++ {
-			j := i + d
-			for j < 0 {
-				j += N
-			}
-			for j >= N {
-				j -= N
-			}
-			in := in[j]
-			k := kern[C-d]
-			out[i] += in * k
+			x := (2 * float64(d) * delta / width)
+			kern[C-d] = math.Exp(-x * x)
 		}
+
+		// convolution: out = in * kern
+		for i := range out {
+			for d := -C; d <= C; d++ {
+				j := i + d
+				for j < 0 {
+					j += N
+				}
+				for j >= N {
+					j -= N
+				}
+				in := in[j]
+				k := kern[C-d]
+				out[i] += in * k
+			}
+		}
+	} else {
+		out = in
 	}
 
 	// determine stddev
